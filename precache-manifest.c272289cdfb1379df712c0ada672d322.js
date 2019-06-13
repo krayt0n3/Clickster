bf7d53986d37f3547ab99fd64942042ac147e367
r8yt <hendersonkevin64@gmail.com>
2019-06-12 21:54:43 -0500

    Updates

---
+++ precache-manifest.c272289cdfb1379df712c0ada672d322.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64945855a33f15311bdae924037a6ece",
+    "url": "/Clickster/index.html"
+  },
+  {
+    "revision": "e8625c1adf7ca1a89260",
+    "url": "/Clickster/static/css/main.2e8ed099.chunk.css"
+  },
+  {
+    "revision": "fc9afafb808ec1d26220",
+    "url": "/Clickster/static/js/2.febb934a.chunk.js"
+  },
+  {
+    "revision": "e8625c1adf7ca1a89260",
+    "url": "/Clickster/static/js/main.4a073136.chunk.js"
+  },
+  {
+    "revision": "4ef01a6ab06f54882430",
+    "url": "/Clickster/static/js/runtime~main.970e4a92.js"
+  }
+]);
